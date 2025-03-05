de9a4c0eadf5ece4cef3c1aa72f7f47fd23c3dbf
z3y50n <dim_papag@windowslive.com>
2025-03-05 13:41:10 +0200

    Project 4

--- 04_machine_language/Fill/Fill.asm
+++ 04_machine_language/Fill/Fill.asm
@@ -7,5 +7,67 @@
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
+	
+	@status
+	M=-1
+	@newstatus
+	M=0 
+	@CHECKCHANGE
+	0;JMP
+(KBDLOOP)
+	@KBD
+	D=M
+	@SETWHITE
+	D;JEQ
+	@SETBLACK
+	0;JMP
+(SETBLACK)
+	@newstatus
+	M=-1
+	@CHECKCHANGE
+	0;JMP
+(SETWHITE)
+	@newstatus
+	M=0
+	@CHECKCHANGE
+	0;JMP
+(CHECKCHANGE)
+	@status
+	D=M
+	@newstatus
+	D=D-M
+	@KBDLOOP
+	D;JEQ
+(SETSCREEN)
+	@newstatus
+	D=M
+	@status
+	M=D
 
-//// Replace this comment with your code.
+	@SCREEN
+	D=A
+	@8191
+	D=D+A
+	@position
+	M=D
+(PAINTLOOP)
+	@position
+	D=M
+	@SCREEN
+	D=D-A
+	@KBDLOOP
+	D;JLT
+
+	@status
+	D=M
+	@position
+	A=M
+	M=D
+	@position
+	M=M-1
+	@PAINTLOOP
+	0;JMP
+	
+
+	
+
--- 04_machine_language/Mult/Mult.asm
+++ 04_machine_language/Mult/Mult.asm
@@ -7,4 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+    @R2
+    M=0
+    @i
+    M=1
+(LOOP)
+    @i
+    D=M
+    @R0
+    D=D-M
+    @END
+    D;JGT
+
+    @R1
+    D=M
+    @R2
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
