952228a731b9584f3a784a5244ca01639a03e1f6
z3y50n <dim_papag@windowslive.com>
2025-03-05 10:58:49 +0200

    Project 4 layout and examples

---
+++ 04_machine_language/PointerDemo.asm
@@ -0,0 +1,22 @@
+    @n
+    M=0
+(LOOP)
+    @n
+    D=M
+    @R1
+    D=D-M
+    @END
+    D;JEQ
+    @R0
+    D=M
+    @n
+    A=D+M
+    M=-1
+    @n
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
+    
